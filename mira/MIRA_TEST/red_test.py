6cfef4b7e28c3c0fcc70654f9d44451922e0e0f1
Apersant1 <miracles.on.the.turn>
2019-04-18 23:19:44 +0400

    Add test home_page

--- mira/MIRA_TEST/red_test.py
+++ mira/MIRA_TEST/red_test.py
@@ -1,5 +1,7 @@
 
 import pytest
+from django.urls import resolve
+from django.test import TestCase
 from selenium import webdriver
 
 @pytest.fixture
@@ -11,6 +13,11 @@ def browser():
 def test(browser):
     browser.get('http://127.0.01:8000/home/')
     assert browser.title == 'Привет,я Мира'
+
+class HomePageTest(TestCase):
+    def Test_When_open_home_page(self):
+        response = self.client.get('/home/')
+        self.assertTemplateUsed(response, "home/home.html")
 
 
 
