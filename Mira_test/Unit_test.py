f8aa578a4a6574f1d102f045c69082bffcbaf6fb
Apersant1 <miracles.on.the.turn>
2019-04-18 20:31:06 +0400

    End UnitTest's

---
+++ Mira_test/Unit_test.py
@@ -0,0 +1,21 @@
+import unittest
+from selenium import webdriver
+
+class NewClientTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+
+        self.browser.quit()
+
+    def test_can(self):
+        self.browser.get('http://127.0.01:8000/home/')
+        self.assertIn("Привет,я Мира" , self.browser.title)
+
+
+
+if  __name__ == '__main__':
+    unittest.main(warnings='ignore')
